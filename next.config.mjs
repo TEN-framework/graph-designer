674daabaa891b9d79f4278a34151b714c9ff4d2e
zhangqianze <zhangqianze@agora.io>
2024-09-04 14:40:39 +0800

    fix: update

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
-const { TEN_DEV_SERVER_URL } = process.env;
-
-// Check if environment variables are available
-if (!TEN_DEV_SERVER_URL) {
-  throw "Environment variables TEN_DEV_SERVER_URL are not available";
-}
-
 const nextConfig = {
   // basePath: '/ai-agent',
   // output: 'test',
   output: "standalone",
   reactStrictMode: false,
   async rewrites() {
+    const { TEN_DEV_SERVER_URL } = process.env;
+
+    // Check if environment variables are available
+    if (!TEN_DEV_SERVER_URL) {
+      throw "Environment variables TEN_DEV_SERVER_URL are not available";
+    }
     return [
       {
         source: '/api/dev-server/v1/:path*',
