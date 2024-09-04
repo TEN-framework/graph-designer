261463a10db5b678bf2aa0bc1fc8c5b1170f62a5
zhangqianze <zhangqianze@agora.io>
2024-09-04 13:54:19 +0800

    feat: replace front-end request with next.js api proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,24 @@
 /** @type {import('next').NextConfig} */
+const { TEN_DEV_SERVER_URL } = process.env;
+
+// Check if environment variables are available
+if (!TEN_DEV_SERVER_URL) {
+  throw "Environment variables TEN_DEV_SERVER_URL are not available";
+}
+
 const nextConfig = {
   // basePath: '/ai-agent',
   // output: 'test',
   output: "standalone",
   reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/dev-server/v1/:path*',
+        destination: `${TEN_DEV_SERVER_URL}/api/dev-server/v1/:path*`,
+      },
+    ];
+  },
 }
 
 export default nextConfig
